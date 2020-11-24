7a03a31b5c17b8f0348c1c411e1b5792cdeabde3
Marina Weis <marinaweisproductions@gmail.com>
2020-11-24 16:37:46 -0500

    no errors - displaying background

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    };
+  }
+  renderBox(i) {
+    return <Box value="something in a string" />;
+  }
+  render() {
+    return (
+      <div>
+        <div className="Body"></div>
+        {this.renderBox()}
+      </div>)
+  }
+}
+
+class Box extends React.Component {
+
+  render() {
+    return (
+      <div className="Box">
+        {this.props.value}
+      </div>
+
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+    // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
